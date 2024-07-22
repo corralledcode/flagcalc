5c877c8f04ebfd34b56e4b95d2e412d2019f17c6
Peter Glenn <160734215+peterglenn116@users.noreply.github.com>
2024-07-22 10:24:28 -0400

    Add files via upload

--- scripts/testsuite.sh
+++ scripts/testsuite.sh
@@ -18,5 +18,5 @@ $PTH/flagcalc -d out4.dat -a s="[Knc]([cliquem])" all -v i=minimal3.cfg
 $PTH/flagcalc -r 10 18 100 -a a="[radiusm]" s="[connc]([radiusm])" all -v i=minimal3.cfg
 $PTH/flagcalc -r 10 18 100 -a a="[radiusm]" all -v i=minimal3.cfg
 $PTH/flagcalc -r 10 12 50000 -a c=cr1 s2="[cyclet](5) <= 2^5" all -v i=minimal3.cfg
-
+$PTH/flagcalc -r 12 20 5000 -a nc=forestc s2="[girthm] <= (2*[diamm] + 1)" all -v i=minimal3.cfg
 
