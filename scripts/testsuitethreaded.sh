3e275bfe35b6669180280f0b3d9f169281d982c8
Peter Glenn <160734215+peterglenn116@users.noreply.github.com>
2025-04-30 13:19:39 -0400

    Add files via upload

--- scripts/testsuitethreaded.sh
+++ scripts/testsuitethreaded.sh
@@ -9,5 +9,5 @@ $PTH/flagcalc -d f="ab cde fghi jklmn" -a e="THREADED PARTITION (u,v IN V, w AS 
 $PTH/flagcalc -d f="ab cde fghi jklmn" -a e="THREADED PARTITION (u,v IN V, w AS u+10, connvc(u,v))" all -v set allsets i=minimal3.cfg
 
 $PTH/flagcalc -d f="a bc" -a e="THREADED PARTITION (a,b IN Ps(V), st(a) > 0, st(a) == st(b))" all -v set allsets i=minimal3.cfg
-$PTH/flagcalc -d f="ab cde" -a s="THREADED PARTITION (a,b IN Ps(V), st(a) > 0, st(a) == st(b) == PARTITION (a,b IN Ps(V), st(a) > 0, st(a) == st(b)))" all -v set allsets i=minimal3.cfg
+$PTH/flagcalc -d f="ab cde" -a s="THREADED PARTITION (a,b IN Ps(V), st(a) > 0, st(a) == st(b)) == PARTITION (a,b IN Ps(V), st(a) > 0, st(a) == st(b))" all -v set allsets i=minimal3.cfg
 
