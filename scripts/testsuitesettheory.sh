2f77c1f1d18325ed358a74a9ea6d27759ced516f
Peter Glenn <160734215+peterglenn116@users.noreply.github.com>
2025-12-15 07:31:09 -0500

    Add files via upload

--- scripts/testsuitesettheory.sh
+++ scripts/testsuitesettheory.sh
@@ -1,97 +1,219 @@
+# This is a comprehensive script running in the ones or tens of minutes depending on the hardware
+# It predates THREADDED, PARTITION, SORT, and GPU stuff
+# It covers some very pertinent theorems from graph theory, and leands heavily on set-theoretic notions and FOL
+
 PTH=${PTH:-'../bin'}
 
+# This is just fun with ceiling and floor, to effect a partition of V into two disjoint subsets.
+# Note the measure "st" returns the size (cardinality) of the set or tuple passed to it as an argument
+# This query should return all 50 "true"
 $PTH/flagcalc -r 9 2 50 -a s="EXISTS (t IN Ps(V), EXISTS (s IN Ps(V), (s CUP t) == V AND st(s) == ceil(dimm/2) AND st(t) == floor(dimm/2)))" all -v i=minimal3.cfg
+
+# This again is just set theory and arithmetic. It should return all 1 true: for each n < graph dim, the correspondingly-sized sets form a partition of V
 $PTH/flagcalc -r 10 10 1 -a s="FORALL (n IN NN(dimm), EXISTS (s IN Ps(V), EXISTS (t IN Ps(V), (s CUP t) == V AND st(s) == ceil(dimm/(n+1)) AND st(t) == floor(n*dimm/(n+1)))))" all -v i=minimal3.cfg
+
+# This is more set theory around the definition of the set of edges E: each e IN E consists of exactly two distinct items. Note that e IN E is not a tuple but a set
 $PTH/flagcalc -r 9 10 100 -a s="FORALL (e IN E, EXISTS (a IN V, EXISTS (b IN V, a ELT e AND b ELT e AND a != b)))" all -v i=minimal3.cfg
-$PTH/flagcalc -d halfconnectedgraph.dat -a s="EXISTS (s IN Ps(V), st(s) >= dimm/2 AND FORALL (a IN s, FORALL (b IN s, a == b OR EXISTS (e IN E, a ELT e AND b ELT e))))" all
+
+# "There is a subset of V having size at least half of graph dim, such that the subset is completely connected.
+# Inspecting the data file halfconnectedgraph.dat, we see this should return "True" for four out of the five graphs in that file
+$PTH/flagcalc -d halfconnectedgraph.dat -a s="EXISTS (s IN Ps(V), st(s) >= dimm/2 AND FORALL (a IN s, FORALL (b IN s, a == b OR EXISTS (e IN E, a ELT e AND b ELT e))))" all -v i=minimal3.cfg
+
+# These last few have all used an EXISTS to test what can more succinctly be tested by invoking measure "ac", taking two inputs and returning True precisely if the two vertices are connected
+# In the present case, we are testing that the measure "Knc" is correct: Knc takes two arguments n and mincnt and returns true if there are at least mincnt embeddings of K_n into the graph
+# Here: all of the ten random graphs should return True
 $PTH/flagcalc -r 10 20 10 -a s="FORALL (n IN NN(dimm+1), Knc(n,1) IFF EXISTS (s IN Sizedsubset(V,n), FORALL (b IN s, FORALL (a IN s, a == b OR EXISTS (e IN E, a ELT e AND b ELT e)))))" all -v i=minimal3.cfg
+
+# This is half a thought: if a subset s is not all of V, then there are two subsets of V not subsets of the subset s such that the meet of s and (any) one of them is contained in s
+# called "half a thought" because it isn't invoking u meaningfully; one can find inspiration, though, and try several variations on this query
 $PTH/flagcalc -r 9 15 1 -a s="FORALL (s IN Ps(V), s == V OR EXISTS (t IN Ps(V), EXISTS (u IN Ps(V), (NOT (u <= s)) AND (NOT (t <= s)) AND ((s CAP t) <= s))))" all
+
+# This is a way to test a graph for being complete bipartite: it should return true for the duplicate two graphs in the named data file
 $PTH/flagcalc -d testbip10.dat -a s="EXISTS (s IN Ps(V), EXISTS (t IN Ps(V), ((s CAP t) == Nulls) AND ((s CUP t) == V) AND FORALL (a IN s, FORALL (b IN s, NOT ac(a,b))) AND FORALL (d IN t, FORALL (c IN t, NOT ac(c,d)))))" all
+
+# This is elementary set theory: it states that E is a subset of the powerset of V: it should return true for all fifty graphs
 $PTH/flagcalc -r 4 5 50 -a s="EXISTS (s IN Ps(Ps(V)), s == E)" all -v i=minimal3.cfg
+
+# This is more elementary set theory: for every edge e there is a set a amongst subsets of V sized 2, such that e is the set consisting of two elements from a
+# Please note one can also just say "a == e"
 $PTH/flagcalc -r 6 10 2 -a s="FORALL (e IN E, EXISTS (a IN Sizedsubset(V,2), FORALL (b IN a, FORALL (c IN a, (b ELT e) AND (c ELT e)))))" all -v i=minimal3.cfg
+
+# This is another way to state the previous more succinctly
 $PTH/flagcalc -r 6 10 2 -a s="FORALL (e IN E, e ELT Sizedsubset(V,2))" all -v i=minimal3.cfg
+
+# This states that there is a set of sized-two subsets of V that essentially equals E: all 8 should return True
 $PTH/flagcalc -r 6 10 8 -a s="EXISTS (s IN Ps(Sizedsubset(V,2)), FORALL (a IN s, FORALL (b IN a, FORALL (c IN a, EXISTS (e IN E, c ELT e AND b ELT e)))))" all -v i=minimal3.cfg
+
+# Again, the previous but more succinctly
 $PTH/flagcalc -r 6 10 8 -a s="EXISTS (s IN Ps(Sizedsubset(V,2)), FORALL (a IN s, a ELT E))" all -v i=minimal3.cfg
+
+# Some subset of the set of subsets of V has size 2^dimm: since the size of Ps(V) is 2^dimm, this is
+# pointing out that one of the subsets of Ps(V) is indeed all of Ps(V); it should return True
 $PTH/flagcalc -r 4 3 1 -a s="EXISTS (s IN Ps(Ps(V)), st(s) == 2^dimm)" all
+
+# This simply states (by way of continuing to affirm the language and its parser) that every subset of subsets of V
+# either has some element, or is sized 0. Note using "ELT" inline, but "IN" for quantifying;
+# this is a departure from received notation, where the epsilon is used for both; it helps with syntax and parsing
 $PTH/flagcalc -r 4 3 1 -a s="FORALL (s IN Ps(Ps(V)), EXISTS (t IN Ps(V), t ELT s) OR st(s) == 0)" all
+
+# This again is more affirmation from elementary set theory: some subset of size equal to the size of the edges set E
+# of the set of 2-sized subsets of V, is equal to the edge set E; it should return True for all ten
 $PTH/flagcalc -r 7 8 10 -a s="EXISTS (s IN Sizedsubset(Sizedsubset(V,2),st(E)), s == E)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 8 10 -a s="EXISTS (s IN Ps(Sizedsubset(V,2)), s == E)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d testbip12.dat -a s="EXISTS (n IN NN(dimm+1), EXISTS (l IN Sizedsubset(V,n), EXISTS (r IN Sizedsubset(V,dimm - n), st(r CUP l) == dimm AND FORALL (a IN l, FORALL (b IN l, NOT ac(a,b))) AND FORALL (c IN r, FORALL (d IN r, NOT ac(c,d))))))" all
+
 $PTH/flagcalc -r 4 3 10 -a s="FORALL (n IN NN(2^dimm+1), EXISTS (s IN Sizedsubset(Ps(V),n), EXISTS (t IN Sizedsubset(Ps(V),2^dimm - n), ((s CUP t) == Ps(V)) AND ((s CAP t) == Nulls))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 10 20 100 -a s="FORALL (e IN E, ac(idxt(e,0),idxt(e,1)))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 10 20 100 -a s="FORALL (n IN NN(st(E)), ac(idxt(idxs(E,n),0),idxt(idxs(E,n),1)))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 12 10 -a s="FORALL (b IN V, FORALL (a IN V, st(Pathss(a,b)) == pct(a,b)))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 6 10 10 -a s="FORALL (b IN V, FORALL (a IN V, FORALL (p IN Pathss(a,b), FORALL (m IN NN(st(p)-1), ac(idxt(p,m),idxt(p,m+1))))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d testbip8.dat -a s="FORALL (v IN V, FORALL (c IN Cyclesvs(v), mod(st(c),2) == 0))" all
+
 $PTH/flagcalc -r 8 10 1000 -a s="cr1 IFF FORALL (v IN V, FORALL (c IN Cyclesvs(v), st(c) > 3))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 9 8 100 -a s="treec IFF FORALL (a IN V, FORALL (b IN V, st(Pathss(a,b)) == 1))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 9 12 100 -a s="forestc IFF FORALL (a IN V, FORALL (b IN V, st(Pathss(a,b)) <= 1))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 9 12 100 -a s="forestc IFF FORALL (a IN V, st(Cyclesvs(a)) == 0)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 9 6 100 -a s="forestc IFF FORALL (a IN V, st(Cyclesvs(a)) == 0)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 10 15 50 -a s="conn1c IFF FORALL (a IN V, FORALL (b IN V, Pathss(a,b) != Nulls))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10.5 50 -a s="FORALL (v IN V, FORALL (c IN Cyclesvs(v), FORALL (w IN c, TupletoSet(c) ELT Cyclesvs(w))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10.5 50 -a s="FORALL (v IN V, FORALL (c IN Cyclesvs(v), FORALL (w IN c, TupletoSet(c) ELT (SET (d IN Cyclesvs(w), TupletoSet(d))))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d testbip10.dat -a s="EXISTS (r IN Ps(V), EXISTS (l IN Ps(V), (l CUP r) == V AND bipc(l,r)))" all
+
 $PTH/flagcalc -r 9 10 100 -a s="(EXISTS (r IN Ps(V), EXISTS (l IN Ps(V), (l CUP r) == V AND bipc(l,r)))) IFF FORALL (v IN V, FORALL (c IN Cyclesvs(v), mod(st(c),2) == 0))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10 100 -a s="SUM (n IN NN(dimm+1), cyclet(n)) == SUM (v IN V, SUM (c IN Cyclesvs(v), 1/st(c)))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 12 33 1000 -a s="SUM (v IN V, vdt(v))/2 == edgecm" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10 100 -a s="SUM (e IN E, 1) == edgecm" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 8 10 100 -a s="FORALL (v IN V, st(Cyclesvs(v)) == cyclesvt(v))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10 1000 -a s="FORALL (v IN V, FORALL (c IN Cyclesvs(v), SUM (a IN c, SUM (b IN c, ac(b,a)))/2 >= lt(c)))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10.5 50 -a s="FORALL (v IN V, FORALL (c IN Cyclesvs(v), FORALL (w IN c, EXISTS (d IN Cyclesvs(w), TupletoSet(d) == TupletoSet(c)))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a s="EXISTS (c IN Cyclesvs(0), EXISTS (d IN Cyclesvs(0), c != d AND TupletoSet(c) == TupletoSet(d)))" all
+
 $PTH/flagcalc -d f="abc" -a s="EXISTS (c IN Cyclesvs(0), EXISTS (d IN Cyclesvs(0), c != d AND TupletoSet(c) == TupletoSet(d)))" all
+
 $PTH/flagcalc -r 20 95 100 -a s="TALLY (v IN V, vdt(v))/2 == edgecm" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 15 52.5 10000 -a s="COUNT (v IN V, vdt(v) % 2 == 1) % 2 == 0" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 15 52.5 10000 -a i="COUNT (v IN V, vdt(v) % 2 == 1)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 6 7.5 1 -a e="Cyclesvs(0)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a p="idxs(Pathss(0,1),0)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcdef" -a p="idxs(Pathss(0,5),0)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -r 10 22.5 1 -a e="V CUP E" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="Pathss(0,2) CUP Pathss(0,3)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcde" -a e="V CUP E" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc" -a e="Pathss(0,1) CUP Pathss(0,2)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="BIGCUPD (v IN V, Cyclesvs(v))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -r 8 14 100 -g o=out.dat all overwrite -v i=minimal3.cfg
+
 $PTH/flagcalc -d out.dat -a e="BIGCUPD (v IN V, Cyclesvs(v))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -d out.dat -a e="BIGCUP (v IN V, Cyclesvs(v))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -d out.dat -a e="BIGCUP (v IN V, BIGCUP (c IN Cyclesvs(v), TupletoSet(c)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), TupletoSet(c)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc d" -a e="SET (x IN V, st(SET (e IN E, x ELT e, e)) > 0, x)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd=efgh" -a e="BIGCUPD (v IN V, SET (c IN Cyclesvs(v), st(c) % 2 == 1, TupletoSet(c)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd=efgh" -a e="BIGCUPD (v IN V, SET (c IN Cyclesvs(v), st(c) % 2 == 0, TupletoSet(c)))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd=efgh" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), st(c) % 2 == 0, TupletoSet(c)))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd=efgh" -a a="SUM (v IN V, vdt(v) > dimm/2, 1)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 16 60 100 -a s="SUM (v IN V, vdt(v) > dimm/4, 1) == COUNT (v IN V, vdt(v) > dimm/4)" all -v i=minimal3.cfg
+
 $PTH/flagcalc -r 16 60 1000 -a s="FORALL (v IN V, vdt(v) > 0, EXISTS (u IN V, ac(u,v)))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc" -a e="SET (v IN V, SET (u IN V, SET (t IN V, t)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc" -a e="SETD (v IN V, SETD (u IN V, SETD (t IN V, t)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (a IN c, EXISTS (b IN c, a ELT e AND b ELT e AND a != b)), e)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), idxt(c,n) ELT e AND idxt(c,(n + 1) % st(c)) ELT e), e)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -r 8 14 100 -a s="cr1" e2="BIGCUP (v IN V, Cyclesvs(v))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="-abcdea" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcde" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e)))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -r 8 14 100 -a f="abcd" a2="cliquem>3" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcde" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e)))" all -v set i=minimal3.cfg
+
 $PTH/flagcalc -r 4 3 1 -a s="FORALL (s IN Ps(Ps(V)), EXISTS (t IN Ps(V), t ELT s) OR st(s) == 0)" all
+
 $PTH/flagcalc -d f="abc" -a e="SET (v IN V, SET (u IN V, SET (t IN V, t)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -r 7 10 100 -a s="SUM (n IN NN(dimm+1), cyclet(n)) == st(BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcde" -a s="SUM (n IN NN(dimm+1), cyclet(n)) == st(BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e))))" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcde" -a e="{3+2,SUM (v IN V, v), {1}}" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc=def=ghi" -a e="BIGCUP (v IN V, {v+1})" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc=def=ghi" -a e="SET (v IN V, {v+1})" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc=def=ghi" -a p="<<0,<<<<1>>,5,6>>,2,3>>" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd=ef=ghi" -a e="{SET (v IN V, FORALL (c IN Cyclesvs(v), st(c) % 2 == 0), v)}" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd=efghi" -a e="{SET (v IN V, FORALL (c IN Cyclesvs(v), (st(c) % 2) == 0), v)}" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="-abcdefdgha" -a e="Cyclesvs(0)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="-abcdefdgha" -a e="BIGCUP (v IN V, SET (c IN Cyclesvs(v), SET (e IN E, EXISTS (n IN NN(st(c)), c[n] ELT e AND c[(n+1)%st(c)] ELT e), e)))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcde" -a e="SET (es IN Ps(E), st(es) != 0, (es[0])[0])" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcdef" -a a="MAX (n IN NN(st(Cyclesvs(0)[0])), (Cyclesvs(0)[0])[n])" all -v i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc=def=ghi" -a a="<<0,2, SUM (v IN Ps(V), st(v) > 0, st(Cyclesvs(v[0])))>>[1]" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc=def=ghi" -a a="<<0,2, SUM (v IN Ps(V), st(v) > 0, st(Cyclesvs(v[0])))>>[2]" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abc=def=ghi" -a e="SETD (n IN st(V), SETD (i IN n, {i}))" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -d f="abcd" -a e="SET (s IN Setpartition(V), s)" all -v set allsets i=minimal3.cfg
+
 $PTH/flagcalc -r 7 20 100 -a s="conn1c" s2="Knc(dimm,1)" s3="EXISTS (c IN Setpartition(V), st(c) == Deltam AND FORALL (s IN c, FORALL (u1 IN s, FORALL (u2 IN s, NOT ac(u1,u2)))))" all -v i=minimal3.cfg
 
 # Diestel 5.2.4 (Brooks 1941)
@@ -283,3 +405,5 @@ $PTH/flagcalc -r 50 20 1 -a e="PARTITION (u,v IN V, connvc(u,v))" all -v set all
 # 5:57 runtime 5/1/2025 on same
 
 # 5:23 runtime 5/1/2025 after idealizeset
+
+# 4:20 on a Threadripper 24 core Shimada Peak series 12/12/2025
